82587a70c6941c2720575a49cb98ded82d26a901
Guzalia Sirazhitdinova <gummiyami@mail.ru>
2021-05-28 02:44:41 +0500

    Форматирование файла testcmp.cpp

--- testcmp.cpp
+++ testcmp.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-int main(){//тестирование класса комплексных чисел
+int main()
+{//тестирование класса комплексных чисел
     Complex A, B(-4), C(23.0, 45.9);
-
     cout << A << "," << B << "," << C <<endl;
     A = B + C;
     cout << "A = B + C , A = " << A << endl;
@@ -19,7 +19,7 @@ int main(){//тестирование класса комплексных чисел
   	cout << "Enter complex number D = ";
   	cin >> D;
     A + = C + D;
-    cout << "D = " << D << "\ n A = " << A << endl;
+    cout << "D = " << D << "\nA = " << A << endl;
 
     return 0;
 }
